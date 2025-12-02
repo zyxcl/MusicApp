e5d7497aba83cda284a3721d1ad2ee6c3ad5be14
赵亚祥 <1358293899@qq.com>
2025-12-02 17:28:14 +0800

    修改url

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,6 +1,6 @@
 
-const BaseURL = 'https://zyxcl.xyz/music/api'
-// const BaseURL = 'http://121.89.213.194:5001'
+const BaseURL = 'https://music.zyxcl.xyz'
+// const BaseURL = 'http://39.96.210.90:5001'
 
 let loadingCount = 0
 
--- src/services/index.ts
+++
@@ -1,6 +0,0 @@
-import request from './request'
-import type { BannerRes } from './type'
-
-export const getBannerApi = () => {
-  return request<BannerRes>({ url: 'https://zyxcl.xyz/music/api/banner' })
-}
