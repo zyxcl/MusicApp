dc8199964543d3e7aa03d2973b7c3d614e0c3bb3
赵亚祥 <1358293899@qq.com>
2025-12-05 14:36:04 +0800

    fix

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -18,20 +18,28 @@ homepageApi().then(res => {
   console.log(res.data.blocks)
   blocks.value = res.data.blocks
 })
-	
+
+const showType = {
+  BANNER: 'BANNER',
+  DRAGON_BALL: 'DRAGON_BALL',
+  HOMEPAGE_SLIDE_PLAYLIST: 'HOMEPAGE_SLIDE_PLAYLIST',
+  HOMEPAGE_SLIDE_SONGLIST_ALIGN: 'HOMEPAGE_SLIDE_SONGLIST_ALIGN'
+} as const
+
 </script>
 <template>
   <playerBar>
     <Header />
     <view class="block" v-for="block in blocks" :key="block.blockCode">
       <!-- 轮播 -->
-      <Banner v-if="block.showType === 'BANNER'" :banners="block.extInfo.banners" />
+      <Banner v-if="block.showType === showType.BANNER" :banners="block.extInfo.banners" />
       <!-- icon图标 -->
-      <Ball v-else-if="block.showType === 'DRAGON_BALL'" />
+      <Ball v-else-if="block.showType === showType.DRAGON_BALL" />
       <!-- 歌单 -->
-      <Playlist v-else-if="block.showType === 'HOMEPAGE_SLIDE_PLAYLIST'" :title="block.uiElement.subTitle.title" :list="block.creatives" />
+      <Playlist v-else-if="block.showType === showType.HOMEPAGE_SLIDE_PLAYLIST" :title="block.uiElement.subTitle.title" :list="block.creatives" />
       <!-- 歌曲 -->
-      <Songs v-else-if="block.showType === 'HOMEPAGE_SLIDE_SONGLIST_ALIGN'" :title="block.uiElement.subTitle.title" :list="block.creatives" :ids="block.resourceIdList" />
+      <Songs v-else-if="block.showType === showType.HOMEPAGE_SLIDE_SONGLIST_ALIGN" :title="block.uiElement.subTitle.title" :list="block.creatives" :ids="block.resourceIdList" />
+      
     </view>
   </playerBar>
 
