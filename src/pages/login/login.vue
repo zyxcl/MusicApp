9ae540a14e80500a9156c1f4365895499ac1bac0
赵亚祥 <1358293899@qq.com>
2025-12-05 11:49:23 +0800

    fix

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -4,10 +4,10 @@ import Qr from './components/Qr.vue'
 import Phone from './components/Phone.vue'
 import { ref } from 'vue'
 
-const loginType = ref('phone') // phone || email || qr
+const loginType = ref('qr') // phone || email || qr
 const typeArr = [
-  { text: '手机号登录', type: 'phone' },
-  { text: '邮箱登录', type: 'email' },
+  // { text: '手机号登录', type: 'phone' },
+  // { text: '邮箱登录', type: 'email' },
   { text: '二维码登录', type: 'qr' }
 ]
 </script>
